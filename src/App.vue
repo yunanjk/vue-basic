f95d4ca810672fa222e126fad6cff39a4a9dedbf
oyy <oyy0821@chunjae.co.kr>
2023-11-17 15:55:42 +0900

    data, methods, binding 수정

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>
+    <!-- {{ person.name }} {{ person.age }} -->
+    {{ nextYear('안녕하세요') }}
+  </p>
+  <p>
+    <!-- <input v-bind:type="inputType" v-bind:value="inputData"> -->
+    <input :type="inputType" :value="inputData">
+  </p>
+  <a :href="getKossieCoderLink('kossiecoder')">코지 코더 채널</a>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+      return {
+        person: {
+        name: '코지 코더',
+        age: 34
+      },
+      inputValue: 'hello',
+      inputType: 'text',
+      aLink: 'https://www.youtube.com/'
+      }
+    },
+  methods: {
+    // 방법1
+    // nextYear: function(greeting) {
+    //   return (
+    //     greeting + '! ' + this.person.name + '는 내년에 ' + (this.person.age + 1) + '살 입니다'
+    //   );
+    // },
+
+    // 방법2
+    nextYear(greeting) {
+      return (
+        greeting + '! ' + this.person.name + '는 내년에 ' + (this.person.age + 1) + '살 입니다'
+      );
+    },
+    otherMethod() {
+      this.nextYear();
+    },
+    getKossieCoderLink(channel) {
+      return this.aLink + channel;
+    }
   }
 }
 </script>
